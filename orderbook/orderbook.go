35105f8d8db57c3ee4770b4b8186eb3bd01fc45d
Andrew Jones <ascjones@gmail.com>
2014-04-06 14:56:07 +0100

    began to implement order matching, just a commit to get it on github

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -1,19 +1,46 @@
 package orderbook
 
+import (
+	"time"
+)
+
 type Order struct {
-	Pair  string
-	Price float64 // be careful with floating point arithmetic losing precision
+	Pair     string
+	Price    float64 // be careful with floating point arithmetic losing precision
+	Quantity float64
+}
+
+type Trade struct {
+	Pair      string
+	Price     float64
+	Quantity  float64
+	Timestamp time.Time
 }
 
 type OrderBook struct {
 	Bids, Asks []Order
+	Trades     []Trade
 }
 
 func NewOrderBook() *OrderBook {
-	return &OrderBook{make([]Order, 0), make([]Order, 0)}
+	return &OrderBook{make([]Order, 0), make([]Order, 0), make([]Trade, 0)}
 }
 
-func (book *OrderBook) PlaceBuyOrder(pair string, price float64) {
-	order := Order{pair, price}
+func (book *OrderBook) PlaceBuyOrder(pair string, price float64, quantity float64) {
+	for i := 0; i < len(book.Asks); i++ {
+		sell := book.Asks[i]
+		if sell.Price == price && sell.Quantity == quantity {
+			// a = append(a[:i], a[i+1:]...)
+			// remove the ask order
+			book.Asks = append(book.Asks[:i], book.Asks[i+1])
+			book.Trades = append(book.Trades, Trade{pair, price, quantity, time.Now()})
+			return
+		}
+	}
+	order := Order{pair, price, quantity}
 	book.Bids = append(book.Bids, order)
+}
+
+func (book *OrderBook) PlaceSellOrder(pair string, price float64, quantity float64) {
+
 }
--- orderbook/orderbook_test.go
+++ orderbook/orderbook_test.go
@@ -23,12 +23,32 @@ var _ = Describe("Orderbook", func() {
 		Context("with no matching sell orders", func() {
 
 			BeforeEach(func() {
-				book.PlaceBuyOrder("BTCUSD", 1000)
+				book.PlaceBuyOrder("BTCUSD", 1000, 1)
 			})
 
 			It("should create a new bid order", func() {
 				Expect(book.Bids).To(HaveLen(1))
-				Expect(book.Bids[0]).To(Equal(Order{"BTCUSD", 1000}))
+				Expect(book.Bids[0]).To(Equal(Order{"BTCUSD", 1000, 1}))
+			})
+		})
+
+		Context("with a fully matching ask order", func() {
+
+			BeforeEach(func() {
+				book.PlaceSellOrder("BTCUSD", 1000, 1)
+				book.PlaceBuyOrder("BTCUSD", 1000, 1)
+			})
+
+			It("doesn't add a buy order", func() {
+				Expect(book.Bids).To(HaveLen(0))
+			})
+
+			It("removes the matching ask order", func() {
+				Expect(book.Asks).To(HaveLen(0))
+			})
+
+			It("should create a new executed trade", func() {
+				Expect(book.Trades).To(HaveLen(1))
 			})
 		})
 	})
